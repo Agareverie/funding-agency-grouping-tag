bb1755b9a059a373370420f172f53a45e5f02e4a
Agari <agareverie@gmail.com>
2025-12-01 18:32:19 +0700

    Path changes and integrating the jupyter notebook into the repo

--- src/utils.py
+++ src/utils.py
@@ -1,7 +1,7 @@
 from pathlib import Path
 from typing import Literal
 
-RESOURCES_DIR = Path("resources")
+RESOURCES_DIR = Path(__file__).parent.parent / "resources"
 
 PLOTS_DIR = RESOURCES_DIR / "plots"
 assert PLOTS_DIR.is_dir()
@@ -15,5 +15,9 @@ assert MODELS_DIR.is_dir()
 TF_IDF_MODEL = MODELS_DIR / "tf_idf.pkl"
 K_MEANS_MODEL = MODELS_DIR / "k_means.pkl"
 
+GRANT_DATA_CSV = DATA_DIR / "processed" / "grant_data.csv"
+GRANT_CLUSTERED_DATA_CSV = DATA_DIR / "processed" / "grant_clustered_data.csv"
+
+
 def scopus_of_year(year: Literal[2018, 2019, 2020, 2021, 2022, 2023]) -> Path:
-    return DATA_DIR / f"{year}.json"
+    return DATA_DIR / "raw" / f"{year}.json"
