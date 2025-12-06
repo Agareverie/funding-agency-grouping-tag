d232d97dce6a5c543e24b44d9c4d58d4755d97cb
Agari <agareverie@gmail.com>
2025-12-07 06:15:22 +0700

    Removal of models directory as it's not needed

--- src/utils.py
+++ src/utils.py
@@ -9,15 +9,8 @@ assert PLOTS_DIR.is_dir()
 DATA_DIR = RESOURCES_DIR / "data"
 assert DATA_DIR.is_dir()
 
-MODELS_DIR = RESOURCES_DIR / "models"
-assert MODELS_DIR.is_dir()
-
-TF_IDF_MODEL = MODELS_DIR / "tf_idf.pkl"
-K_MEANS_MODEL = MODELS_DIR / "k_means.pkl"
-
 GRANT_DATA_CSV = DATA_DIR / "processed" / "grant_data.csv"
 GRANT_CLUSTERED_DATA_CSV = DATA_DIR / "processed" / "grant_clustered_data.csv"
-
 
 def scopus_of_year(year: Literal[2018, 2019, 2020, 2021, 2022, 2023]) -> Path:
     return DATA_DIR / "raw" / f"{year}.json"
